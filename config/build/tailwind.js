212f738e9d46578645ac8827c8cb062523baf0ae
gramirez22 <gramirez22@mac.com>
2021-06-14 13:00:39 -0700

    Typography updates: phase 1 - quick stubs and  tailwind config setup
    - note: I haven't tested any of this yet, most likely broken css. Pushing these stubs up for now until I can get my local working properly.

--- config/build/tailwind.js
+++ config/build/tailwind.js
@@ -9,7 +9,7 @@ let generateSpacing = function(start, end, multiple = 1) {
 	let spacingObject = {};
 	const pixelValues = Array.from(new Array(end - start + 1), (v, k) => k + start).filter((n) => n % multiple === 0);
 	pixelValues.forEach((val) => {
-		spacingObject[val] = convertToRem(val);
+		spacingObject[val] = val + 'px'; //convertToRem(val);
 	});
 	return spacingObject;
 };
@@ -29,6 +29,20 @@ module.exports = {
 			...theme('iconSizes'),
 			'0': '0px',
 			px: '1px',
+			's1': '4px',
+			's2': '8px',
+			's3': '12px',
+			's4': '16px',
+			's5': '20px',
+			's6': '24px',
+			's7': '32px',
+			's8': '40px',
+			's9': '48px',
+			's10': '64px',
+			's11': '80px',
+			's12': '96px',
+			's13': '120px',
+			's14': '160px',
 			...generateSpacing(1, 35),
 			...generateSpacing(35, 75, 5),
 			...generateSpacing(80, 150, 10),
