3d067397eb3256c561068014a3a51c78ecd6960f
gramirez22 <gramirez22@mac.com>
2021-06-16 08:59:31 -0700

    removing twigpack basic setup
    - this will become its own branch

--- config/craft/general.php
+++ config/craft/general.php
@@ -13,11 +13,6 @@ use craft\helpers\App;
 return [
     // Global settings
     '*' => [
-
-        'aliases' => [
-            '@webroot' => CRAFT_BASE_PATH . '/web',
-        ],
-        
         // Default Week Start Day (0 = Sunday, 1 = Monday...)
         'defaultWeekStartDay' => 1,
 
