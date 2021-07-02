3d3066de9933ee13701b0235fbc8ae7b6d4cde1f
jakedohm <jake.dohm@me.com>
2021-07-02 17:49:04 -0400

    Adding icon component

---
+++ src/js/components/Icons.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg class="icon-svg" :class="[this._size.heightClass, this._size.widthClass]">
+    <use :xlink:href="path"></use>
+  </svg>
+</template>
+
+<script>
+const sizes = {
+  tiny: { abbr: 't', heightClass: 'h-icon-tiny', widthClass: 'w-icon-tiny' },
+  xs: { abbr: 'xs', heightClass: 'h-icon-xs', widthClass: 'w-icon-xs' }
+}
+function sizeExists(size) {
+  return Object.keys(sizes).includes(size)
+}
+
+export default {
+  props: {
+    name: String,
+    size: {
+      tiny: String,
+      validator: sizeExists
+    },
+  },
+  computed: {
+    _size() {
+      return sizes[this.size]
+    },
+    path() {
+      const sizeAbbreviation = this._size.abbr
+      return `/assets/icons/${sizeAbbreviation}-${this.name}.svg#icon-definition`
+    }
+  }
+}
+</script>
