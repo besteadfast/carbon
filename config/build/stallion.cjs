5a57d519954f745c207f7d3b01d7618b12cc6926
jakedohm <jake.dohm@me.com>
2023-04-13 09:20:05 -0400

    Adding Tailwind colors for base styles

--- config/build/stallion.cjs
+++ config/build/stallion.cjs
@@ -2,19 +2,31 @@ const colors = {
 	white: '#ffffff',
 	black: '#000000',
 
-	// Example brand color
+	// Default Tailwind colors
+	gray: {
+    50: '#f9fafb',
+    100: '#f3f4f6',
+    200: '#e5e7eb',
+    300: '#d1d5db',
+    400: '#9ca3af',
+    500: '#6b7280',
+    600: '#4b5563',
+    700: '#374151',
+    800: '#1f2937',
+    900: '#111827',
+  },
 	blue: {
-		900: '#ffffff',
-		800: '#ffffff',
-		700: '#ffffff',
-		600: '#ffffff',
-		500: '#ffffff',
-		400: '#ffffff',
-		300: '#ffffff',
-		200: '#ffffff',
-		100: '#ffffff',
-		50: '#ffffff',
-	},
+    50: '#eff6ff',
+    100: '#dbeafe',
+    200: '#bfdbfe',
+    300: '#93c5fd',
+    400: '#60a5fa',
+    500: '#3b82f6',
+    600: '#2563eb',
+    700: '#1d4ed8',
+    800: '#1e40af',
+    900: '#1e3a8a',
+  },
 }
 
 const icons = {
