37d9fc3e79b6caeaf97dcec865f1320b3f72b72e
Benjamin Bunze <btbunze@live.unc.edu>
2024-10-18 11:19:39 -0400

    Fix Safari open/close issue

--- src/js/components/library/AccordionItem.vue
+++ src/js/components/library/AccordionItem.vue
@@ -2,7 +2,7 @@
 	<details
 		class="accordion-item border-x border-b border-gray-200 first:rounded-t last:rounded-b first:border-t"
 		as="div"
-		:open="open || isClosing"
+		:open="open || isClosing || isOpening"
 	>
 		<summary
 			class="flex justify-between items-center w-full px-s4 py-s2 bg-transparent interact:bg-blue-100 interact:outline-none transition duration-300 hover:cursor-pointer"
@@ -14,7 +14,7 @@
 					class="transition duration-300"
 					:class="open ? 'rotate-180' : 'rotate-0'"
 				>
-					<svg fill="#000000" height="12px" width="12px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+					<svg fill="#000000" height="12px" width="12px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
 						viewBox="0 0 330 330" xml:space="preserve">
 					<path id="XMLID_225_" d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
 						c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
@@ -24,8 +24,9 @@
 			</slot>
 		</summary>
 		<div
-			class="max-h-0 transition-all overflow-hidden"
+			class="transition-all overflow-hidden"
 			ref="accPanelRef"
+			style="max-height:0;"
 		>
 			<slot name="content">
 				<div class="p-s4 h-[100px] flex items-center">Accordion Body</div>
@@ -60,6 +61,7 @@ onMounted(() => {
 			isClosing.value = false
 		} else if (isOpening.value) {
 			isOpening.value = false
+			accPanelRef.value.style = ""
 		}
 	})
 })
@@ -69,15 +71,21 @@ watch(open, (open, oldOpen) => {
 	if (open) {
 		content.style.maxHeight = content.scrollHeight + 'px'
 		isOpening.value = true
-		if (scrollOnOpen) {
-			document.documentElement.scrollTo({
-				top:
-					accButtonRef.value.getBoundingClientRect().top + window.scrollY - 20,
-			})
-		}
+		setTimeout(() => {
+			content.style.maxHeight = content.scrollHeight + 'px'
+			if (scrollOnOpen) {
+				document.documentElement.scrollTo({
+					top:
+						accButtonRef.value.getBoundingClientRect().top + window.scrollY - 20,
+				})
+			}
+		}, 0);
 	} else {
-		content.style.maxHeight = 0
 		isClosing.value = true
+		content.style.maxHeight = content.scrollHeight + 'px'
+		setTimeout(() => {
+			content.style.maxHeight = 0
+		}, 0)
 	}
 })
 </script>
